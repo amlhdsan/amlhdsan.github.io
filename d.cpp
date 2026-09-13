69b700af1a5fcb8e9fc811da416d15d598997710
amlhdsan <151382553+amlhdsan@users.noreply.github.com>
2026-09-13 14:17:56 +0800

    Update d.cpp

--- d.cpp
+++ d.cpp
@@ -207,5 +207,85 @@ int main() {
     return 0;
 }
 
+// 電影發明以後，人類的生命，比以前至少延長了三倍。
+// amlhdsan
+#include <bits/stdc++.h>
+#define N 200010
+using namespace std;
+
+int n;
+long long s, a[N];
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') {
+            f = -1;
+        }
+        ch = getchar();
+    }
+    while(ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if(x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if(x > 9) {
+        write(x / 10);
+    }
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+int main() {
+    int T = read();
+
+    while(T--) {
+        n = read();
+        s = 0;
+
+        for(int i = 1; i <= n; i++) {
+            a[i] = read();
+            s += a[i];
+        }
+
+        if(n == 1) {
+            writeln(0);
+            continue;
+        }
+
+        if(n == 2) {
+            long long d = llabs(a[1] - a[2]);
+            d %= 4;
+
+            if(d == 3) {
+                d = 1;
+            }
+
+            writeln(d);
+            continue;
+        }
+
+        if(n % 2 == 0 && (s & 1)) {
+            writeln(1);
+        } else {
+            writeln(0);
+        }
+    }
+
+    return 0;
+}
+
 
 
